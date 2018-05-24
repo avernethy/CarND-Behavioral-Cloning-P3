13f8d58c3c9f371ebea66341fecd17eaa0572c1b
Vern Francisco <avernethy@gmail.com>
2018-05-23 23:58:35 -0500

    normalized input

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open("vern_data/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -12,24 +12,28 @@ images = []
 measurements = []
 for line in lines:
     source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    #print(source_path)
+    filename = source_path.split("\\")[-1]
+    current_path = 'vern_data/IMG/' + filename
     image = cv2.imread(current_path)
+    #print(filename)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train - np.array(images)
-y_train = np.aprray(measurements)
-
-from keras.models. import Sequential
-from keras.layers import Flatten, Dense
+X_train = np.array(images)
+y_train = np.array(measurements)
+input_shape = X_train.shape
+print(X_train)
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
 
 model.save('model.h5')
